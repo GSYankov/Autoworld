0efc8bd4f94c3800c9718b1bcd22d22a78a4d749
Georgi Yankov <spookyfox@abv.bg>
2020-11-14 17:25:57 +0200

    Present simple file form

--- app/views.py
+++ app/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.forms import ImageForm
 
 # Create your views here.
 
+
 def home(request):
-   return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def login(request):
-   return render(request, 'login.html')
+    return render(request, 'login.html')
+
 
 def customer(request):
-   return render(request, 'home_loggedin_customer.html')
+    return render(request, 'home_loggedin_customer.html')
+
 
 def file_form(request):
-   return render(request, 'file_form.html')
+    if request.method == 'GET':
+        context = {
+            'form': ImageForm
+        }
+        return render(request, 'file_form.html', context)
+    else:
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save()
+            image.save()
+            return redirect('home')
+
+        context = {
+            'form': form
+        }
+
+        return render(request, 'file_form.html', context)
--- autoworld/settings.py
+++ autoworld/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = join(BASE_DIR, 'media')
